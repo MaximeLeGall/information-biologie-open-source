2bb0d15fd8f3fe8e150b8d4014b5dad5414e13fa
maxime <maximelegall@ymail.com>
2020-05-23 11:47:49 +0200

    add class in function page open

--- page-php/header.php
+++ page-php/header.php
@@ -1,13 +1,14 @@
 <!DOCTYPE HTML PUBLIC "-//W3C//DTD HTML 4.01 Transitional//EN" "http://www.w3.org/TR/html4/loose.dtd">
 <html>
     <head>
-        <pre>
-            <?php print_r($_SERVER) ?>
-        </pre>
         <?php
-            $itemSelected = substr($_SERVER['SCRIPT_NAME'], 1); 
-        ?>
-        <link rel="stylesheet" type="text/css" href="articles.css">
+            $itemSelected = explode("/", $_SERVER['SCRIPT_NAME']);
+            if(in_array("acceuil", $itemSelected)):?>
+                <link rel="stylesheet" type="text/css" href="acceuil.css">
+            <?php elseif(in_array("article", $itemSelected)):?>
+                <link rel="stylesheet" type="text/css" href="articles.css">
+            <?php endif?>
+        
         <title>vieillissement</title>
     </head>
     <body> 
@@ -18,7 +19,7 @@
         <header>
             <nav class="menu">
                 <ul>
-                    <li><a href="/acceuil/acceuil.php">acceuil</a></li>
+                    <li><a href="http://localhost:8000/acceuil/acceuil.php">acceuil</a></li>
                     <li><a href="#">cathégorie</a></li>
                     <li><a href="#">news</a></li>
                     <li><a href="#">matériel</a></li>
