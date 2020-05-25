d895eec61107210b1b7f2cd15da8c5d56e7ea4c4
maxime <maximelegall@ymail.com>
2020-05-26 00:01:22 +0200

    add header-footer.css file

--- page-php/header.php
+++ page-php/header.php
@@ -1,6 +1,7 @@
-<!DOCTYPE HTML PUBLIC "-//W3C//DTD HTML 4.01 Transitional//EN" "http://www.w3.org/TR/html4/loose.dtd">
+<!DOCTYPE html PUBLIC "-//W3C//DTD HTML 4.01 Transitional//EN" "http://www.w3.org/TR/html4/loose.dtd">
 <html>
     <head>
+        <link rel="stylesheet" type="text/css" href="../page-php/header-footer.css">
         <?php
                 // add style file
             $itemSelected = explode("/", $_SERVER['SCRIPT_NAME']);
