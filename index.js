f85876089e4d8db3d3cb584fcfd3c15548f8ea75
maxime <maximelegall@ymail.com>
2020-08-03 23:51:47 +0200

    feat (first part): ajout du responsive et correction bug d'affichage lors du redimensionnage

--- index.js
+++ index.js
@@ -2,50 +2,52 @@
 
 //switch carousel first-part
 (function(){
-    var firstPart, 
-    firstPart_width,
-    article,
-    article_width,
-    article_all,
-    article_last_index,
-    arrowChangeArticle,
-    arrowChangeArticle_width,
-    activeArticle_lenght,
-    allActiveArticle_width,
-    element;
+    var firstPart_width,
+        article_width,
+        article_all,
+        article_last_index,
+        allActiveArticle_width,
+        allActiveArticle_center,
+        inlineArticle,
+        activeArticle_all,
+        element;
     var visibleArticle = function(){
-        firstPart = document.querySelector("#first-part");
-        firstPart_width = firstPart.getBoundingClientRect().width;
-        article = document.querySelector(".inlineArticle div");
-        article_width = article.getBoundingClientRect().width + 30;
+        firstPart_width = document.querySelector("#first-part").getBoundingClientRect().width;
+        inlineArticle = document.querySelector(".inlineArticle");
+        article_width = document.querySelector(".inlineArticle div").getBoundingClientRect().width + 30;
         article_all = document.querySelectorAll(".inlineArticle div");
         article_last_index = article_all.length;
-        activearticle_lenght = 0;
-        arrowChangeArticle_width = document.querySelector(".arrowChangeArticle").getBoundingClientRect().width * 2;
         for(i = 0; i < article_last_index; i++){
             element = article_all[i];
-            if(article_width * (i + 1) + arrowChangeArticle_width < firstPart_width){
+            if(article_width * (i + 1) + 100 < firstPart_width){
                 element.classList.remove("hideArticle");
                 element.classList.add("visibleArticle");
-                activeArticle_lenght += 1;
             } 
             else{
                 element.classList.remove("visibleArticle");
                 element.classList.add("hideArticle");
             }
         }
-        allActiveArticle_width = activeArticle_lenght * article_width;
+            //centrage des éléments actifs
+        activeArticle_all = document.querySelectorAll(".visibleArticle");
+        allActiveArticle_width = activeArticle_all.length * article_width;
+        allActiveArticle_center = (firstPart_width - allActiveArticle_width) / 2;
+        inlineArticle.style.marginLeft = allActiveArticle_center + "px";
+            //retour au première élément
+        if(lastArticle_width != undefined){
+            lastArticle_width = 0;
+            inlineArticle.style.transform = "translateX(" + lastArticle_width + "px)";
+        }
     }
     visibleArticle();
     window.addEventListener("resize", visibleArticle);
         
 
     var lastArticle_width = 0;
-    var inlineArticle = document.querySelector(".inlineArticle");
-    var activeArticle_all = document.querySelectorAll(".visibleArticle");
-    var activeArticle_last;
-    var activeArticle_first;
-    var article_invisible_width = article_width * (article_last_index - 1 - activeArticle_all.length);
+    var activeArticle_all,
+        activeArticle_last,
+        activeArticle_first,
+        article_invisible_width;
     function nextArticle(){
         if(lastArticle_width < 0){
             activeArticle_all = document.querySelectorAll(".visibleArticle");
@@ -61,10 +63,11 @@
     }
 
     function previousArticle(){
-        if( - lastArticle_width <= article_invisible_width){
-            activeArticle_all = document.querySelectorAll(".visibleArticle");
-            activeArticle_first = activeArticle_all[0];
-            activeArticle_last = activeArticle_all[activeArticle_all.length - 1];
+        activeArticle_all = document.querySelectorAll(".visibleArticle");
+        activeArticle_first = activeArticle_all[0];
+        activeArticle_last = activeArticle_all[activeArticle_all.length - 1];
+        article_invisible_width = article_width * (article_last_index - 1 - activeArticle_all.length);
+        if( - lastArticle_width <= article_invisible_width && activeArticle_last.nextElementSibling != undefined){
             lastArticle_width = lastArticle_width - article_width;
             inlineArticle.style.transform = "translateX(" + lastArticle_width +"px)";
             activeArticle_first.classList.remove("visibleArticle");
